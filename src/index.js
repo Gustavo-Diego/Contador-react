5f0aab58d69f8cbf1befadac966ac80808c118fd
Gustavo-Diego <53282378+Gustavo-Diego@users.noreply.github.com>
2019-07-31 18:18:09 -0300

    1.0.2
    ATUALIZADO

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
@@ -17,6 +17,11 @@ const contar = () => {
   let f = Number(fim.value);
   let pas = Number(passo.value);
 
+  if (pas < 0) {
+    pas = 1;
+    alert("passo será considerado 1");
+  }
+
   if (
     inicio.value.length === 0 ||
     fim.value.length === 0 ||
@@ -25,6 +30,8 @@ const contar = () => {
     alert("erro: preencha todos os dados!");
   } else if (ini > f) {
     alert("erro: Fim não pode ser menor que inicio!");
+  } else if ((f - ini) / pas > 10000) {
+    alert("Informe uma diferença de valores menor para melhor visualização!");
   } else {
     for (ini; ini <= f; ini += pas) {
       res.innerHTML += ini + "\u{1F603}";
@@ -34,31 +41,48 @@ const contar = () => {
 };
 
 const limpar = () => {
-  let inicio = (document.getElementById("txtinicio").value = "");
-  let fim = (document.getElementById("txtfim").value = "");
-  let passo = (document.getElementById("txtpasso").value = "");
+  document.getElementById("txtinicio").value = "";
+  document.getElementById("txtfim").value = "";
+  document.getElementById("txtpasso").value = "";
   let res = document.getElementById("res");
 
   res.innerHTML = "contando...";
-  let ini = document.getElementById("txtinicio").focus();
+  document.getElementById("txtinicio").focus();
 };
 
 function App() {
+  const [estado, setEstado] = useState("START");
+
+  const contando = () => {
+    setEstado("contando");
+  };
+
+  const voltar = () => {
+    setEstado("START");
+  };
+
+  if (estado === "START") {
+    return <button onClick={contando}>Vamos contar ?</button>;
+  }
   return (
     <div className="App">
       {titulo}
       <p>
-        Inicio:<input type="Number" name="inicio" id="txtinicio" />
+        Inicio:
+        <input type="Number" name="inicio" id="txtinicio" />
       </p>
       <p>
-        Fim:<input type="Number" name="fim" id="txtfim" />
+        Fim:
+        <input type="Number" name="fim" id="txtfim" />
       </p>
       <p>
-        Passo:<input type="Number" name="passo" id="txtpasso" min="1" />
+        Passo:
+        <input type="Number" name="passo" id="txtpasso" min="1" />
       </p>
       <p>
         <button onClick={contar}>Contar</button>
         <button onClick={limpar}>Limpar</button>
+        <button onClick={voltar}>Voltar</button>
       </p>
       {rest}
     </div>
